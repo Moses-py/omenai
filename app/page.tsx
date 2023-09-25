89fcc7c2603ca8c163ea4a9a60b178d09b796dba
Moses Chukwunekwu <dantereus1@gmail.com>
2023-09-25 15:09:08 +0100

    Minor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Coming soon!</p>
+      <Link href={"/auth/register/individual"}>
+        <button
+          className={` rounded-full px-[1.5rem] py-[0.4rem] mt-[1rem] bg-secondary text-white hover:bg-secondary/30 transition-all ease-linear duration-200`}
+          type={"button"}
+        >
+          Individual signup
+        </button>
+      </Link>
+      <Link href={"/auth/register/gallery"}>
+        <button
+          className={` rounded-full px-[1.5rem] py-[0.4rem] mt-[1rem] bg-primary text-white hover:bg-secondary/30 transition-all ease-linear duration-200`}
+          type={"button"}
+        >
+          Gallery signup
+        </button>
+      </Link>
     </main>
   );
 }
