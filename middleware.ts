024fc5b5ccbb5997ebabf0c95ae0cb3bb759fb61
Olamilekan <nunuolamilekan@gmail.com>
2023-10-10 11:45:13 +0100

    Added multiple middleware structure

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,4 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+import { stackMiddlewares } from "./middlewares/stackMiddlewares";
+import { withAuthorization } from "./middlewares/withAuthorization";
 
-export default withAuth(
-  async function middleware(req) {
-    const token = req.nextauth.token;
-    const pathname = req.nextUrl.pathname;
-
-    if (token) {
-      if (pathname === "/" || isPublicRoutes(pathname)) {
-        return NextResponse.redirect(new URL(`/dashboard`, req.url));
-      }
-      if (!token.verified) {
-        const url = new URL(`/verify/${token.type}/${token.id}`, req.url);
-        return NextResponse.redirect(url);
-      }
-    }
-  },
-  {
-    pages: {
-      signIn: "/",
-    },
-  }
-);
-
-const isPublicRoutes = (pathname: string): boolean => {
-  const publicRoutes: string[] = ["/auth", "/verify"];
-  const isMatch = publicRoutes.find((r) => r.startsWith(pathname));
-
-  if (!isMatch) return false;
-  return true;
-};
-
-export const config = { matcher: ["/dashboard"] };
+export default stackMiddlewares([withAuthorization]);
