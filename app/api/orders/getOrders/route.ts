7ec2930c9a7099791faa21b29a7db79d75777f6a
Moses Chukwunekwu <dantereus1@gmail.com>
2023-11-18 10:57:44 +0100

    FIX: Fixed server component error on deployment

--- app/api/orders/getOrders/route.ts
+++ app/api/orders/getOrders/route.ts
@@ -1,14 +1,20 @@
+import { CreateOrder } from "@/models/orders/CreateOrderSchema";
 import { ServerError } from "@/custom/errors/dictionary/errorDictionary";
 import { handleErrorEdgeCases } from "@/custom/errors/handler/errorHandler";
 import { connectMongoDB } from "@/lib/mongo_connect/mongoConnect";
-import { CreateOrder } from "@/models/orders/CreateOrderSchema";
+
 import { NextResponse } from "next/server";
+import { AccountIndividual } from "@/models/auth/IndividualSchema";
+import { Artworkuploads } from "@/models/artworks/UploadArtworkSchema";
 
 export async function POST(request: Request) {
   try {
     await connectMongoDB();
 
     const { id } = await request.json();
+
+    await AccountIndividual.startSession();
+    await Artworkuploads.startSession();
 
     const orders = await CreateOrder.find({ gallery_id: id })
       .sort({ updatedAt: -1 })
