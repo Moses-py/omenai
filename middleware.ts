cfdf802ac5a6a19fe10a7494d9a55c656a190ee8
Moses Chukwunekwu <dantereus1@gmail.com>
2023-11-19 10:04:07 +0100

    FIX: Fixed MismatchContentLengthError fetch error

--- middleware.ts
+++ middleware.ts
@@ -14,44 +14,43 @@ function redirect(url: string, request: NextRequest) {
   return NextResponse.redirect(new URL(url, request.url));
 }
 export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  if (token) {
-    switch (token.role) {
-      case "gallery":
-        if (
-          galleryLogin.test(request.url) ||
-          galleryRegister.test(request.url)
-        ) {
-          return redirect(URLS.galleryOverview, request);
-        } else if (userDashboardRegex.test(request.url)) {
-          return redirect(URLS.userLogin, request);
-        }
-        break;
-      case "user":
-        if (userLogin.test(request.url) || userRegister.test(request.url)) {
-          return redirect(URLS.userProfile, request);
-        } else if (galleryDashboardRegex.test(request.url)) {
-          return redirect(URLS.galleryLogin, request);
-        }
-        break;
-    }
-    if (verifyPath.test(request.url)) {
-      return redirect(URLS.root, request);
-    }
-  } else {
-    if (userDashboardRegex.test(request.url)) {
-      return redirect(URLS.userLogin, request);
-    } else if (galleryDashboardRegex.test(request.url)) {
-      return redirect(URLS.galleryLogin, request);
-    }
-  }
-  return NextResponse.next();
+  // const token = await getToken({
+  //   req: request,
+  //   secret: process.env.NEXTAUTH_SECRET,
+  // });
+  // if (token) {
+  //   switch (token.role) {
+  //     case "gallery":
+  //       if (
+  //         galleryLogin.test(request.url) ||
+  //         galleryRegister.test(request.url)
+  //       ) {
+  //         return redirect(URLS.galleryOverview, request);
+  //       } else if (userDashboardRegex.test(request.url)) {
+  //         return redirect(URLS.userLogin, request);
+  //       }
+  //       break;
+  //     case "user":
+  //       if (userLogin.test(request.url) || userRegister.test(request.url)) {
+  //         return redirect(URLS.userProfile, request);
+  //       } else if (galleryDashboardRegex.test(request.url)) {
+  //         return redirect(URLS.galleryLogin, request);
+  //       }
+  //       break;
+  //   }
+  //   if (verifyPath.test(request.url)) {
+  //     return redirect(URLS.root, request);
+  //   }
+  // } else {
+  //   if (userDashboardRegex.test(request.url)) {
+  //     return redirect(URLS.userLogin, request);
+  //   } else if (galleryDashboardRegex.test(request.url)) {
+  //     return redirect(URLS.galleryLogin, request);
+  //   }
+  // }
+  // return NextResponse.next();
 }
 
-export const config = {
-  matcher: ["/dashboard/:path*", "/auth/:path*", "/verify/:path*"],
-};
+// export const config = {
+//   matcher: ["/dashboard/:path*", "/auth/:path*", "/verify/:path*"],
+// };
