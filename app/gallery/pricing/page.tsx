d8737879f3704a21b91916241007b148a177836f
Moses Chukwunekwu <dantereus1@gmail.com>
2023-12-26 17:07:00 +0100

    WIP: Built navbar for page layout

--- app/gallery/pricing/page.tsx
+++ app/gallery/pricing/page.tsx
@@ -1,13 +1,13 @@
-import Navbar from "@/components/navbar/Navbar";
 import React from "react";
 import Landing from "./components/Landing";
 import { PackageCard } from "./components/PackageCard";
 import Faq from "./components/Faq";
+import DesktopNavbar from "@/components/navbar/desktop/DesktopNavbar";
 
 export default function PricingPage() {
   return (
     <div className="sm:container p-5">
-      <Navbar />
+      <DesktopNavbar />
       <Landing />
       <PackageCard />
       <Faq />
